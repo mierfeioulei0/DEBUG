e326c29c71243240513ec14389817813a88dd3bd
mierfeioulei0 <371696999@qq.com>
2018-06-09 12:09:32 +0800

    加注释

--- p130.asm
+++ p130.asm
@@ -7,11 +7,11 @@ code segment
 start:
   mov ax,data
   mov ds,ax
-  mov bx,0
-  mov cx,5
+  mov bx,0;设置偏移地址
+  mov cx,5;循环五次
 s:
   mov al,[bx]
-  and al,11011111b
+  and al,11011111b;转为大写字母 大写字母第五位为0
   mov [bx],al
   inc bx
   loop s
@@ -20,7 +20,7 @@ s:
   mov cx,11
 s0:
   mov al,[bx]
-  or  al,00100000b
+  or  al,00100000b;转为小写字母 小写字母第五位为1
   mov [bx],al
   inc bx
   loop s0
